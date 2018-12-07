ecc1ec7bf4aaf3092b9f622b45f3edb55c1c24b4
Aman Mahalgavan <aman.mahalgavan1991@gmail.com>
2018-12-07 10:27:30 +0900

    added Web3 to the app. Modified App.js to capture accounts from the browser and Provide Web3 Object to the Application.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { Button, Card } from '@material-ui/core';
+
+const providerFromWbe3 = Web3.givenProvider;
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allAccounts: []
+    }
+  }
+  getAccounts = () => {
+    web3.eth.getAccounts().then((accounts) => {
+      console.log("Accounts --> ", accounts);
+      console.log("Providers --> ", providerFromWbe3);
+      this.setState({ allAccounts: accounts });
+
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <pre>
+          {`All Accounts --> ` + JSON.stringify(this.state.allAccounts)}
+        </pre>
+        <Card>
+          <Button onClick={this.getAccounts}>
+            GET ALL ACCOUNTS
+            			</Button>
+        </Card>
       </div>
     );
   }
